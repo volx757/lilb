2354225d66ed290e1e8d3bbbc418b5f97eb936b5
volx757 <bendbarclay@gmail.com>
2015-10-15 22:02:25 -0400

    update 2

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
     })
 
     owl.trigger('next.owl.carousel').trigger('next.owl.carousel').trigger('next.owl.carousel')
+
+    $('#thisone').on('click', function () {
+        fbShare('https://www.rarelilb.com', "Rare Lil'B", 'rare facebook posts by the Based God', 'http://localhost:3000/public/wisdom00239.png', 600, 300)
+    })
 })
+function fbShare(url, title, descr, image, winWidth, winHeight) {
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+}
